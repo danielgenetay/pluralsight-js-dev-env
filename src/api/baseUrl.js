0fba441dcd0524841c5030eb00c23846efa46426
Daniel Génetay (DN) <daniel.genetay@DNMC02WK2SBHV2Q.local>
2018-07-19 15:37:29 +0200

    Updated with more settings for production

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,4 +1,15 @@
 export default function getBaseUrl(){
-  const inDevelopment = window.location.hostname === 'localhost';
-  return inDevelopment ? 'http://localhost:3001/' : '/';
+  console.log(getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/'); //eslint-disable-line no-console
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+}
+
+function getQueryStringParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  console.log(results); //eslint-disable-line no-console
+  if(!results) return null;
+  if(!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
